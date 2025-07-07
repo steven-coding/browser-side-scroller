47263f924815d73e3be32d9be2517796e4eb4d05
Steven <steven_thielemann@yahoo.de>
2025-07-07 17:15:09 +0200

    prod build settings

---
+++ vite.config.prod.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/browser-side-scroller',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: false,
+    minify: 'esbuild',
+    target: 'es2020',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['typescript']
+        },
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split('.')
+          const ext = info[info.length - 1]
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(ext)) {
+            return `assets/images/[name]-[hash][extname]`
+          }
+          return `assets/[name]-[hash][extname]`
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js'
+      }
+    },
+    assetsInlineLimit: 4096,
+    cssCodeSplit: true,
+    reportCompressedSize: false
+  }
+})
